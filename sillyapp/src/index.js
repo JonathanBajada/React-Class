cd7075a252d741d63e98c36d7f95ded1da0d0b92
Jonatahn Bajada <jbajada@ryerson.ca>
2020-07-07 14:26:49 -0400

    updated life cycle methods

--- sillyapp/src/index.js
+++ sillyapp/src/index.js
@@ -1,45 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  //not required by react but belongs to js language
-  //first function called before anything else
-  //same props function we use with functional comp
-  constructor(props){
-    super(props);//must call this, con func overrides react.comp, we use super to reference react.comp
-     
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };//our state object
+	//not required by react but belongs to js language
+	//first function called before anything else
+	//same props function we use with functional comp
 
-    //we put this here so that the location is updated before jsx is rendered second time
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => { 
-        console.log(position);
-          this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message})
-        console.log(err)
-      }
-    );
-  }
+	state = { lat: null, errorMessage: '' };
 
-  // React says we have to define render!! if we dont define react with throw error
-  render() {
-      if(this.state.errorMessage && !this.state.lat){
-        return <div>Error: {this.state.errorMessage}</div>
-      }
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
+		);
+	}
 
-      if (!this.state.errorMessage && this.state.lat){
-        return <div>Latitude: {this.state.lat}</div>
-      }
+	// React says we have to define render!! if we dont define react with throw error
+	render() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
 
-      return <div>Loading...</div>
-   
-  }
+		if (!this.state.errorMessage && this.state.lat) {
+			return <SeasonDisplay lat={this.state.lat} />;
+		}
+
+		return <div>Loading...</div>;
+	}
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root')) 
-
+ReactDOM.render(<App />, document.querySelector('#root'));
